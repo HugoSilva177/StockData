49868c51f7a028f779d40ed394c8b400498b6b66
HugoSilva177 <hugo_silva177@hotmail.com>
2021-04-19 22:28:09 -0300

    Refatoração de testes unitários no web_scraping (test_request e test_dao)

--- web_scraping/fundamentus/src/dao/hadoop_hdfs/InfoEmpresaHDFS.py
+++ web_scraping/fundamentus/src/dao/hadoop_hdfs/InfoEmpresaHDFS.py
@@ -3,10 +3,9 @@ from web_scraping.fundamentus.src.dao.hadoop_hdfs.ReadHDFS import ReadHDFS
 
 class InfoEmpresaHDFS(ReadHDFS):
 
-    def __init__(self):
+    def __init__(self, url_complementar="fundamentus/detalhes/info_empresa"):
         self.__app_name = "write_read_fundamentus_hdfs"
-        self.__url_complementar = "fundamentus/detalhes/info_empresa"
-        super().__init__(self.__app_name, self.__url_complementar)
+        super().__init__(self.__app_name, url_complementar)
 
     def buscar_dados_empresa(self, papel, data_cotacao=None):
         dados_spark_df = self._ler_dados_spark_dataframe_no_hdfs()
